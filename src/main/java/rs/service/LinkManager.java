e7720cd3b36e27cabe2a2168fbe891f6c9a8bf85
Vladmir Parshikov <vladimirus@gmail.com>
2015-07-09 16:09:17 +0100

    added debugging

--- src/main/java/rs/service/LinkManager.java
+++ src/main/java/rs/service/LinkManager.java
@@ -1,5 +1,8 @@
 package rs.service;
 
+import static java.lang.String.format;
+
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rs.dao.SimpleDao;
@@ -9,11 +12,13 @@ import java.util.Collection;
 
 @Service
 public class LinkManager implements SimpleManager<Link> {
+    private Logger log = Logger.getLogger(LinkManager.class);
     @Autowired
     private SimpleDao<Link> linkDao;
 
     @Override
     public void save(Link link) {
+        log.debug(format("Saving link %s, score: %d, comments: %d", link.getUrl(), link.getScore(), link.getCommentCount()));
         linkDao.save(link);
     }
 
--- src/main/java/rs/service/TopicManager.java
+++ src/main/java/rs/service/TopicManager.java
@@ -1,6 +1,9 @@
 package rs.service;
 
+import static java.lang.String.format;
+
 import com.google.common.eventbus.AsyncEventBus;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rs.dao.SimpleDao;
@@ -10,6 +13,7 @@ import java.util.Collection;
 
 @Service
 public class TopicManager implements SimpleManager<Topic> {
+    private Logger log = Logger.getLogger(TopicManager.class);
     @Autowired
     private SimpleDao<Topic> topicDao;
     @Autowired
@@ -17,6 +21,7 @@ public class TopicManager implements SimpleManager<Topic> {
 
     @Override
     public void save(Topic topic) {
+        log.debug(format("Saving topic %s, subscribers: %d", topic.getDisplayName(), topic.getSubscribers()));
         topicDao.save(topic);
         eventBus.post(topic);
     }
