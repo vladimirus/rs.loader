8a2f86a4107f9992c6f480560007057e6d2be61a
Vladmir Parshikov <vladimirus@gmail.com>
2015-07-09 15:55:49 +0100

    renamed

--- src/main/java/rs/RSpider.java
+++ src/main/java/rs/RSpider.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableAutoConfiguration
 @SpringBootApplication
-public class RsApplication {
+public class RSpider {
 
     public static void main(String[] args) {
-        SpringApplication.run(RsApplication.class, args);
+        SpringApplication.run(RSpider.class, args);
     }
 }
--- src/test/java/rs/RSpiderTests.java
+++ src/test/java/rs/RSpiderTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = RsApplication.class)
-public class RsApplicationTests {
+@SpringApplicationConfiguration(classes = RSpider.class)
+public class RSpiderTests {
 	@Test
 	public void contextLoads() {
     }
