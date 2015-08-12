31f7ea1b7f445afc923c95958942686298abcf0d
Vladmir Parshikov <vladimirus@gmail.com>
2015-08-12 13:13:33 +0100

    added metrics

--- src/main/java/rs/job/AbstractLoaderJob.java
+++ src/main/java/rs/job/AbstractLoaderJob.java
@@ -3,6 +3,8 @@ package rs.job;
 import static java.util.stream.Collectors.toList;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import rs.service.convert.Converter;
 
 import java.util.Collection;
@@ -10,6 +12,8 @@ import java.util.stream.Stream;
 
 public abstract class AbstractLoaderJob<F, T> {
     Logger log = Logger.getLogger(AbstractLoaderJob.class);
+    @Autowired
+    private CounterService counterService;
 
     Collection<T> load(Stream<F> stream, Converter<F, T> converter) {
         return stream.map(original -> {
@@ -21,6 +25,7 @@ public abstract class AbstractLoaderJob<F, T> {
                     }
                 })
                 .filter(out -> out != null)
+                .peek(out -> counterService.increment("loader." + out.getClass().getSimpleName().toLowerCase()))
                 .collect(toList());
     }
 }
