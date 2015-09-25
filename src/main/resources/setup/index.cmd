347a5e9db7335fc2a8c0c4b640a6da34e2bb2841
Vladmir Parshikov <vladimirus@gmail.com>
2015-09-25 14:16:04 +0100

    usefull commands

--- src/main/resources/setup/index.cmd
+++ src/main/resources/setup/index.cmd
@@ -1,1 +1,40 @@
+curl -XDELETE "http://localhost:9200/rs/"
+
 curl -XPUT "http://localhost:9200/rs/"
+
+curl -XPUT "http://localhost:9200/rs/_mapping/topic" -d'
+{
+  "topic": {
+    "_timestamp": {
+      "enabled": true,
+      "store": true
+    },
+    "properties": {
+      "created": {
+        "type": "date"
+      },
+      "updated": {
+        "type": "date"
+      }
+    }
+  }
+}'
+
+curl -XPUT "http://localhost:9200/rs/_mapping/link" -d'
+{
+  "link": {
+    "_timestamp": {
+      "enabled": true,
+      "store": true
+    },
+    "properties": {
+      "title": {
+        "type": "string",
+        "analyzer": "english"
+      },
+      "created": {
+        "type": "date"
+      }
+    }
+  }
+}'
