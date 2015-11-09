9e1632526f80b0c92c14be7c8a3f306b26c363c0
Vladmir Parshikov <vladimirus@gmail.com>
2015-11-09 17:29:24 +0000

    refactoring

--- src/main/java/rs/loader/job/LinkLoaderJob.java
+++ src/main/java/rs/loader/job/LinkLoaderJob.java
@@ -66,7 +66,7 @@ public class LinkLoaderJob extends AbstractLoaderJob<Submission, Link> {
             timer.start();
 
             ofNullable(queue.poll())
-                    .flatMap(topicDisplayName -> process(topicDisplayName, 200))
+                    .flatMap(topicDisplayName -> process(topicDisplayName, 100))
                     .filter(links -> !links.isEmpty())
                     .ifPresent(links -> {
                         timer.stop();
